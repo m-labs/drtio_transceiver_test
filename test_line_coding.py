292ca3131e4f0bd49cc1b507d313d626325ded42
Sebastien Bourdeauducq <sb@m-labs.hk>
2016-07-24 14:52:15 +0800

    line_coding: more tests, all passing

--- test_line_coding.py
+++ test_line_coding.py
@@ -32,17 +32,53 @@ def encode_sequence(seq):
     return output[2:]
 
 
+def decode_sequence(seq):
+    output = []
+
+    dut = line_coding.Decoder()
+    def pump():
+        for w in seq:
+            yield dut.input.eq(w)
+            yield
+            if (yield dut.k):
+                output.append(Control((yield dut.d)))
+            else:
+                output.append((yield dut.d))
+        yield
+        if (yield dut.k):
+            output.append(Control((yield dut.d)))
+        else:
+            output.append((yield dut.d))
+    run_simulation(dut, pump())
+    return output[1:]
+
+
 class TestLineCoding(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         prng = random.Random(42)
-        cls.input_sequence = [Control((1 << 5) | 28)]*2 
-        cls.input_sequence += [prng.randrange(256) for _ in range(1000)]
+        cls.input_sequence = []
+        for i in range(8):
+            cls.input_sequence += [Control((i << 5) | 28)]*2
+        cls.input_sequence += [prng.randrange(256) for _ in range(10000)]
         cls.output_sequence = encode_sequence(cls.input_sequence)
 
     def test_comma(self):
-        self.assertEqual(self.output_sequence[0], 0b0011111001)
-        self.assertEqual(self.output_sequence[1], 0b1100000110)
+        control_chars = [
+            0b0011110100,
+            0b0011111001,
+            0b0011110101,
+            0b0011110011,
+            0b0011110010,
+            0b0011111010,
+            0b0011110110,
+            0b0011111000,
+        ]
+        for i, c in enumerate(control_chars):
+            ok = {c, ~c & 0b1111111111}
+            with self.subTest(i=i):
+                self.assertIn(self.output_sequence[2*i], ok)
+                self.assertIn(self.output_sequence[2*i+1], ok)
 
     def test_running_disparity(self):
         rd = -1
@@ -51,9 +87,13 @@ class TestLineCoding(unittest.TestCase):
             self.assertIn(rd, {-1, 1})
 
     def test_no_spurious_commas(self):
-        for w1, w2 in zip(self.output_sequence[2:], self.output_sequence[3:]):
+        for w1, w2 in zip(self.output_sequence[16:], self.output_sequence[17:]):
             for shift in range(10):
                 cw = (w1 << shift) | (w2 >> (10-shift))
                 self.assertNotIn(cw, {0b0011111001, 0b1100000110,   # K28.1
                                       0b0011111010, 0b1100000101,   # K28.5
                                       0b0011111000, 0b1100000111})  # K28.7
+
+    def test_roundtrip(self):
+        self.assertEqual(self.input_sequence,
+                         decode_sequence(self.output_sequence))
