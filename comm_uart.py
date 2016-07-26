7af1f7d87efdfe28b154882a1f160dc0e4c85535
Sebastien Bourdeauducq <sb@m-labs.hk>
2016-07-26 15:05:08 +0800

    comm_uart: cleanup

--- comm_uart.py
+++ comm_uart.py
@@ -1,5 +1,8 @@
+import logging
 import serial
-import struct
+
+
+logger = logging.getLogger(__name__)
 
 
 class CommUART:
@@ -7,46 +10,26 @@ class CommUART:
         "write": 0x01,
         "read":  0x02
     }
-    def __init__(self, port, baudrate=115200, debug=False):
-        self.port = port
-        self.baudrate = str(baudrate)
-        self.debug = debug
+    def __init__(self, port, baudrate=115200):
         self.port = serial.serial_for_url(port, baudrate)
 
-    def open(self):
-        if hasattr(self, "port"):
-            return
-        self.port.open()
-
     def close(self):
-        if not hasattr(self, "port"):
-            return
         self.port.close()
-        del self.port
 
-    def _read(self, length):
-        r = bytes()
-        while len(r) < length:
-            r += self.port.read(length - len(r))
-        return r
+    def __enter__(self):
+        return self
 
-    def _write(self, data):
-        remaining = len(data)
-        pos = 0
-        while remaining:
-            written = self.port.write(data[pos:])
-            remaining -= written
-            pos += written
+    def __exit__(self, type, value, traceback):
+        self.close()
 
     def read(self, addr, length=None):
         data = []
         length_int = 1 if length is None else length
-        self._write([self.msg_type["read"], length_int])
-        self._write(list((addr//4).to_bytes(4, byteorder="big")))
+        self.port.write([self.msg_type["read"], length_int])
+        self.port.write((addr//4).to_bytes(4, byteorder="big"))
         for i in range(length_int):
-            value = int.from_bytes(self._read(4), "big")
-            if self.debug:
-                print("read {:08x} @ {:08x}".format(value, addr + 4*i))
+            value = int.from_bytes(self.port.read(4), "big")
+            logger.debug("read %08x @ %08x", value, addr + 4*i)
             if length is None:
                 return value
             data.append(value)
@@ -57,12 +40,11 @@ class CommUART:
         length = len(data)
         offset = 0
         while length:
-            size = min(length, 8)
-            self._write([self.msg_type["write"], size])
-            self._write(list(((addr+offset)//4).to_bytes(4, byteorder="big")))
+            size = min(length, 255)
+            self.port.write([self.msg_type["write"], size])
+            self.port.write(((addr+offset)//4).to_bytes(4, byteorder="big"))
             for i, value in enumerate(data[offset:offset+size]):
-                self._write(list(value.to_bytes(4, byteorder="big")))
-                if self.debug:
-                    print("write {:08x} @ {:08x}".format(value, addr + offset, 4*i))
+                self.port.write(value.to_bytes(4, byteorder="big"))
+                logger.debug("write %08x @ %08x", value, addr + offset, 4*i)
             offset += size
             length -= size
